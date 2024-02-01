09e19ca0469f5b997af4e6adf93d488134344702
Barbara Kosińska <basia7kosinska@.com>
2024-02-01 12:38:02 +0100

    Add test to  convertPLNTuUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,6 +1,12 @@
-export const convertPLNToUSD = (PLN) => {
-  if (typeof PLN === 'string')
+export const convertPLNToUSD = (PLN = 0) => {
+  if (typeof PLN === 'string' || (typeof PLN === 'number' && isNaN(PLN)) || PLN === 0)
   return NaN;
+  
+  if (PLN === null && typeof PLN !== 'number' || typeof PLN === 'function' || (typeof PLN === 'object' && Object.values(PLN).length === 0))
+    return 'Error';
+
+  if (PLN < 0)
+  return '$0.00'
 
   const PLNtoUSD = PLN / 3.5;
   
